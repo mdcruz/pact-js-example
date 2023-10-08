188f4cae226ea07ad6815422a04f2d5a0f9d087f
mdcruz <marie.descruz@gmail.com>
2023-10-08 23:33:45 +0100

    slight refactor

--- consumer/consumer-contract.spec.js
+++ consumer/consumer-contract.spec.js
@@ -23,33 +23,6 @@ describe('Movies Service', () => {
           },
           willRespondWith: {
             status: 200,
-            // body: [
-            //   {
-            //     "id": 1,
-            //     "name": "The Shawshank Redemption",
-            //     "year": 1994
-            //   },
-            //   {
-            //     "id": 2,
-            //     "name": "The Dark Knight",
-            //     "year": 2008
-            //   },
-            //   {
-            //     "id": 3,
-            //     "name": "Amelie",
-            //     "year": 2001
-            //   },
-            //   {
-            //     "id": 4,
-            //     "name": "Fight Club",
-            //     "year": 1994
-            //   },
-            //   {
-            //     "id": 5,
-            //     "name": "The Lion King",
-            //     "year": 1994
-            //   }
-            // ]
             body: eachLike(
               {
                 id: 1,

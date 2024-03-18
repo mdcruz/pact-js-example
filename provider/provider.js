04c98f1f012183d2aeed76043de4ef5d298ac2f4
prescottlewis707 <prescottlewis707@gmail.com>
2024-03-18 23:07:37 +0000

    add swagger docs, integration examples

--- provider/provider.js
+++ provider/provider.js
@@ -1,6 +1,8 @@
-const Joi = require('joi');
 const express = require('express');
-const Movies = require('./movies')
+const Movies = require('./movies');
+const Joi = require('joi');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const server = express();
 server.use(express.json());
@@ -17,10 +19,73 @@ const importData = () => {
   }, 0);
 };
 
+/**
+ * @openapi
+ * /movies:
+ *  get:
+ *    description: Get all movies
+ *    responses:
+ *      '200':
+ *        description: A list of movie objects
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: integer
+ *                  name:
+ *                    type: string
+ *                  year:
+ *                    type: integer
+ *                example:
+ *                  id: 1
+ *                  name: James Bond
+ *                  year: 2021
+ */
 server.get('/movies', (req, res) => {
   res.send(movies.getMovies());
 });
 
+/**
+ * @openapi
+ * /movie/{id}:
+ *  get:
+ *    description: Get a movie by Id
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *    responses:
+ *      '200':
+ *        description: A movie object
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                id:
+ *                  type: integer
+ *                name:
+ *                  type: string
+ *                year:
+ *                  type: integer
+ *              example:
+ *                id: 1
+ *                name: James Bond
+ *                year: 2021
+ *      '404':
+ *        description: No movie exists
+ *        content:
+ *          text/plain:
+ *            schema:
+ *              type: string
+ *              example: Movie not found
+ */
 server.get('/movie/:id', (req, res) => {
   const movie = movies.getMovieById(req.params.id);
   if (!movie) {
@@ -64,6 +129,33 @@ server.delete('/movie/:id', (req, res) => {
     movies.splice(index, 1);
     res.send(`Movie ${req.params.id} has been deleted`);
   }
+});
+
+const options = {
+  failOnErrors: true,
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Provider Movies API"
+    },
+    externalDocs: {
+      description: "swagger.json",
+      url: "/swagger.json"
+    }
+  },
+  apis: ["./provider/provider.js"]
+};
+
+const specs = swaggerJsdoc(options);
+
+server.use(
+  "/swagger",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
+server.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(specs);
 });
 
 module.exports = {
